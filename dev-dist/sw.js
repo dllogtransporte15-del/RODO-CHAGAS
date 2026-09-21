d92639eef014bbbbca8b1cf15bd30607b2990432
Davis <dllogtransporte@gmail.com>
2026-09-21 12:10:56 -0300

    feat: add service worker with workbox precaching and routing

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4a6hbs6q16"
+    "revision": "0.54r34vc063s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
